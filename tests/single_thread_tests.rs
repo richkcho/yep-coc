6584888f0091dfa3e82fdd2cb2f4b66af178dcb5
Richard Cho <richkcho19@gmail.com>
2025-10-06 12:08:36 +0800

    move common test code into utils

--- tests/single_thread_tests.rs
+++ tests/single_thread_tests.rs
@@ -1,28 +1,11 @@
+mod utils;
+
 #[cfg(test)]
 mod tests {
     use yep_coc::{YCQueue, YCQueueError, YCQueueProduceSlot, YCQueueSharedMeta};
     use yep_coc::queue_alloc_helpers::YCQueueOwnedData;
     
-
-    pub fn str_to_u8(s: &str) -> &[u8] {
-        return s.as_bytes()
-    }
-
-    pub fn str_from_u8(buf: &[u8]) -> &str {
-        let len = buf.iter().position(|&b| b == 0).unwrap_or(buf.len());
-        match std::str::from_utf8(&buf[..len]) {
-            Ok(s) => s,
-            Err(e) => panic!("couldn't parse as utf-8 string, err: {:?} buf: {:?}", e, buf),
-        }
-    }
-
-    pub fn copy_str_to_slice(s: &str, buf: &mut [u8]) {
-        let s_bytes = str_to_u8(s);
-        assert!(s_bytes.len() <= buf.len(), "dst buffer not large enough!");
-
-        buf[..s_bytes.len()].copy_from_slice(s_bytes);
-        buf[s_bytes.len()..].fill(0);
-    }
+    use crate::utils::{copy_str_to_slice, str_from_u8};
 
     #[test]
     /**
